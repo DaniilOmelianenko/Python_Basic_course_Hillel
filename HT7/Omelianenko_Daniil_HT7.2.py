f75f037fbe59cb596597d9e34187e06443434471
qa <dr.odiram@gmail.com>
2020-07-22 16:51:41 +0300

    "Initial commit 123Test"

--- HT7/Omelianenko_Daniil_HT7.2.py
+++ HT7/Omelianenko_Daniil_HT7.2.py
@@ -1,5 +1,8 @@
+from collections import Counter
 def converter(string, delimiter):
     temp_list = string.split(delimiter)
+    global Counter
+    print(Counter(temp_list))
     res_dict = dict.fromkeys(temp_list)
     return res_dict
 
