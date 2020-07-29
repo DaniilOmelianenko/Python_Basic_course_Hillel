dc7352df25bcd3935816e63feb0ec41ea7caecf2
DaniilOmelianenko <68437561+DaniilOmelianenko@users.noreply.github.com>
2020-07-29 21:57:48 +0300

    'HT10.1'

---
+++ HT10/Omelianenko_Daniil_HT10.3.py
@@ -0,0 +1,15 @@
+def parse(rules):
+    result = []
+    initial_val = 0
+    for rule in rules:
+        if rule == 'i':
+            initial_val += 1
+        elif rule == 's':
+            initial_val *= initial_val #initial_val **2
+        elif rule == 'd':
+            initial_val -= 1
+        elif rule == 'o':
+            result.append(initial_val) #добавляет значение initial_val в  конец списка result
+    return result
+
+print (parse("iiisdoso"))
