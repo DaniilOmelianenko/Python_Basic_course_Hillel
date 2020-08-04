7dd6ab7632698d4d99d440b23be959a6125f9ff8
DaniilOmelianenko <68437561+DaniilOmelianenko@users.noreply.github.com>
2020-08-04 11:58:36 +0300

    Update Omelianenko_Daniil_HT8_game.py

--- HT8/Omelianenko_Daniil_HT8_game.py
+++ HT8/Omelianenko_Daniil_HT8_game.py
@@ -4,18 +4,17 @@ number_of_attempts = 0
 number = random.randint(1, 50)
 max_attempts = 10
 while number_of_attempts < max_attempts:
-    print("Как ты думаешь, какое? ")
+    number_of_attempts += 1
+    print(f"{number_of_attempts}-я попытка. Как ты думаешь, какое? ")
     guess = input()
     guess = int(guess)
-    number_of_attempts += 1
     if guess < number:
         print("Мое число больше твоего")
-    if guess > number:
+    elif guess > number:
         print("Мое число меньше твоего")
-    if guess == number:
+    elif guess == number:
         break
 if guess == number:
     print(f"Ты угадал число с {number_of_attempts} попытки")
-if guess != number:
-    #number = str(number)
+else:
     print(f"Последняя {max_attempts}-я попытка потрачена! Я загадал число {number}. Ты проиграл!")
