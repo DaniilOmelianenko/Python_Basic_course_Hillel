c7bf105a654af0a96756a0b4e1cfc2a3776e497f
DaniilOmelianenko <68437561+DaniilOmelianenko@users.noreply.github.com>
2020-08-10 13:12:13 +0300

    Update compare_results.py

--- HT9/code_review/compare_results.py
+++ HT9/code_review/compare_results.py
@@ -1,9 +1,7 @@
 def compare(player_select, random_number):
     if player_select == random_number:
-        result = 'Ты угадал моё число!'
+        return 'Поздравляю!!!'
     elif player_select > random_number:
-        result = 'Твоё число БОЛЬШЕ моего! Попробуй снова!'
+        return 'Твоё число БОЛЬШЕ моего! Попробуй снова!'
     elif player_select < random_number:
-        result = 'Твоё число МЕНЬШЕ моего! Попробуй снова!'
-    return result
-
+        return 'Твоё число МЕНЬШЕ моего! Попробуй снова!'
