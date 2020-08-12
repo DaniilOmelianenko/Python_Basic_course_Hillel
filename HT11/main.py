cf18527b078304fd99fde1d54e4ab9386437f849
DaniilOmelianenko <68437561+DaniilOmelianenko@users.noreply.github.com>
2020-08-12 23:44:06 +0300

    'HT11'

--- HT11/main.py
+++ HT11/main.py
@@ -17,7 +17,10 @@ def run_machine():
     result_list = [random.choice(wheel1), random.choice(wheel2), random.choice(wheel3)]
     return result_list
 
-print(run_machine())
+
+run_machine_result = run_machine()
+
+
 
 score_list = {"XXX": (100, 10, 100),
               "A": (90, 9, 18),
@@ -31,7 +34,22 @@ score_list = {"XXX": (100, 10, 100),
               "I": (10, 1, 2)
               }
 
+
 temp_list = {}
-for i in run_machine():
-    temp_list.setdefault(i, run_machine().count(i))
+for i in run_machine_result:
+    temp_list.setdefault(i, run_machine_result.count(i))
 print(temp_list)
+
+for key, value in temp_list.items():
+    if key == 'XXX' and value == 1:
+        xxx = 2
+    else:
+        xxx = 1
+
+#############################
+print(temp_list.values())
+for key, value in temp_list.items():
+    if value == 3:
+        print(score_list[key][0])
+    elif value == 2:
+        print((score_list[key][1]) * xxx)
