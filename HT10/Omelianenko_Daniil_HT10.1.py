d1dbf988d0c91dda88dfdf81aaca61617a74369c
DaniilOmelianenko <68437561+DaniilOmelianenko@users.noreply.github.com>
2020-08-12 13:53:39 +0300

    Update Omelianenko_Daniil_HT10.1.py

--- HT10/Omelianenko_Daniil_HT10.1.py
+++ HT10/Omelianenko_Daniil_HT10.1.py
@@ -1,8 +1,10 @@
-from datetime import datetime
+import datetime
 
 
 def format_datetime(date_string):
-    return datetime.strptime(date_string, "%b %d %Y %I:%M%p")
+    new_date = datetime.datetime.strptime(date_string, "%b %d %Y %I:%M%p")
+    formated_date = new_date.strftime("%Y-%m-%d %H:%M:%S")
+    return formated_date
 
 
 print(format_datetime("Feb 12 2019 2:41PM"))
