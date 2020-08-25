d83c0eb319acdf34122147b175b5de1716f5458c
DaniilOmelianenko <dr.odiram@gmail.com>
2020-08-25 13:41:35 +0300

    ht15

--- HT15/Omelianenko_Daniil_HT15.py
+++ HT15/Omelianenko_Daniil_HT15.py
@@ -1,37 +1,90 @@
 class Figure:
-    def __init__(self, side_1):
-        self.side_1 = side_1
-
-    def perimeter(self):
-        raise NotImplementedError
-
-    def square(self):
-        raise NotImplementedError
+    def __init__(self, side_a):
+        self.side_a = side_a
+        self._sides = [side_a]
 
 
 class Triangle(Figure):
-    def __init__(self, side_1, side_2, side_3):
-        self._validate_figure(side_1, side_2, side_3)
-        super().__init__(side_1)
-        self.side_2 = side_2
-        self.side_3 = side_3
+    def __init__(self, side_a, side_b, side_c):
+        self.__validation_triangle(side_a, side_b, side_c)
+        super().__init__(side_a)
+        self.side_b = side_b
+        self.side_c = side_c
+        self._sides.append(self.side_b)
+        self._sides.append(self.side_c)
 
-    def _validate_figure(self, side_1, side_2, side_3):
-        # какая-то логика по валидации
-        if side_1 <= 0:
-            raise ValueError
-        elif side_2 <= 0:
-            raise ValueError
-        elif side_3 <= 0:
-            raise ValueError
+    def __validation_triangle(self, side_a, side_b, side_c):
+        if (side_a >= (side_b + side_c)) or (side_b >= (side_a + side_c)) or (side_c >= (side_a + side_b)):
+            print("There is no triangle with such sides! Check sides of your triangle!")
 
+    def show_sides(self):
+        return self._sides
+
+    @property
     def perimeter(self):
-        perim = side_1 + side_2 + side_3
-        return perim
+        return self.side_a + self.side_b + self.side_c
 
+    @property
     def square(self):
-        pass
+        p = self.perimeter / 2
+        return round(((p * (p - self.side_a) * (p - self.side_b) * (p - self.side_c)) ** 0.5), 5)
 
 
-figura_1 = Triangle(1, 2, 3)
-print(figura_1.perimeter)
+class Rectangle(Figure):
+    def __init__(self, side_a, side_b):
+        super().__init__(side_a)
+        self.side_b = self.side_d = side_b
+        self.side_c = side_a
+        self._sides.append(self.side_b)
+        self._sides.append(self.side_c)
+        self._sides.append(self.side_d)
+
+    def show_sides(self):
+        return self._sides
+
+    @property
+    def perimeter(self):
+        return (self.side_a + self.side_b) * 2
+
+    @property
+    def square(self):
+        return self.side_a * self.side_b
+
+
+class Square(Figure):
+    def __init__(self, side_a):
+        super().__init__(side_a)
+        self.side_b = self.side_c = self.side_d = side_a
+        self._sides.append(self.side_b)
+        self._sides.append(self.side_c)
+        self._sides.append(self.side_d)
+
+    def show_sides(self):
+        return self._sides
+
+    @property
+    def perimeter(self):
+        return self.side_a * 4
+
+    @property
+    def square(self):
+        return self.side_a ** 2
+
+
+# print('Triangle')
+# # b = Triangle(23, 20, 15)
+# # print(b.show_sides())
+# # print(b.perimeter)
+# # print(b.square)
+#
+# print('Rectangle')
+# # rectangle = Rectangle(10, 5)
+# # print(rectangle.show_sides())
+# # print(rectangle.perimeter)
+# # print(rectangle.square)
+
+print('Square')
+my_square = Square(7)
+print(my_square.show_sides())
+print(my_square.perimeter)
+print(my_square.square)
