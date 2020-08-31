28043007f7dd50c5b237ba575a3b8776a566bcf0
DaniilOmelianenko <dr.odiram@gmail.com>
2020-08-31 18:46:59 +0300

    HT3last

--- HT3/main.py
+++ HT3/main.py
@@ -3,23 +3,17 @@ from input_check_end import input_check_end
 from input_check_delimeter import input_check_delimeter
 
 print("Привет, эта программа покажет все, кратные N числа, в заданном диапазоне!")
-value_a = input_check_start()
-value_b = input_check_end()
-value_c = input_check_delimeter()
-value_d = (int(value_b) + 1)
+start_int = int(input_check_start())
+finish_int = int(input_check_end())
+delimeter = int(input_check_delimeter())
 print(f'''\n{'='*100}''')
-print(f"Сейчас я покажу все числа, кратные {value_c}, в промежутке от {value_a} до {value_b}:")
+print(f"Сейчас я покажу все числа, кратные {delimeter}, в промежутке от {start_int} до {finish_int}:")
 
 if __name__ == '__main__':
-    def multiples():
-        result = []
-        for i in range(value_a, value_d):
-            if (i % int(value_c)) != 0:
-                continue
-            elif i == 0:
-                continue
-            result.append(i)
-        return result
+    def multiples(start, finish, delim):
+        while start % delimeter:
+            start += 1
+        return list(range(start, finish+1, delim))
 
 
-print(multiples())
+print(multiples(start_int, finish_int, delimeter))
