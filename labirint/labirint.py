8bbf63ecc4f46b0f8a16341602c4726c2532c984
DaniilOmelianenko <dr.odiram@gmail.com>
2020-09-28 11:52:10 +0300

    [labirint_fin]

--- labirint/labirint.py
+++ labirint/labirint.py
@@ -12,81 +12,117 @@ labirint = [
     ['#', '#', '#', '#', '.', '#', '#', '#'],
 ]
 steps = []
-a = int(input('vertical: '))
-b = int(input('gorizontal: '))
+a = 5
+b = 6
 start = labirint[a][b]
+# a = int(input('vertical: '))
+# b = int(input('gorizontal: '))
 point = labirint
-start = '#'
 go_left = 'point'
-direction_a = 0
-direction_b = 0
-for i in range(2):
+while True:
     tupik = 0
     exit_point = 0
-    if go_left == 'point' or 'N' and direction_a == 0:
+    while go_left == 'point':
+        if point[a - 1][b] == '.':
+            go_left = 'W'
+            a -= 1
+            if a == 0:
+                exit_point = 1
+                break
+            steps.append([a, b])
+            break
+        elif point[a + 1][b] == '.':
+            go_left = 'E'
+            a += 1
+            if a == -1:
+                exit_point = 1
+                break
+            steps.append([a, b])
+            break
+        elif point[a][b + 1] == '.':
+            go_left = 'N'
+            b += 1
+            if b == -1:
+                exit_point = 1
+                break
+            steps.append([a, b])
+            break
+        elif point[a][b - 1] == '.':
+            go_left = 'S'
+            b -= 1
+            if b == 0:
+                exit_point = 1
+                break
+            steps.append([a, b])
+            break
+    if go_left == 'N' and exit_point != 1:
         while True:
             print(steps)
             if point[a - 1][b] == '.':
                 go_left = 'W'
-                direction_a = 1
                 a -= 1
+                if a == 0 and point[a][b] != start:
+                    exit_point = 1
+                    break
                 steps.append([a, b])
-                tupik = 0
                 break
             elif point[a - 1][b] == '#':
-                tupik += 1
+                go_left = 'E'
                 break
-            else:
-                exit_point = 1
-                break
-    if go_left == 'point' or 'S' and direction_a == 0:
+            # else:
+            #     exit_point = 1
+            #     break
+    if go_left == 'S' and exit_point != 1:
         while True:
             print(steps)
             if point[a + 1][b] == '.':
                 go_left = 'E'
-                direction_a = 1
                 a += 1
+                if a == -1 and point[a][b] != start:
+                    exit_point = 1
+                    break
                 steps.append([a, b])
-                tupik = 0
                 break
             elif point[a + 1][b] == '#':
-                tupik += 1
+                go_left = 'W'
                 break
-            else:
-                exit_point = 1
-                break
-    if go_left == 'point' or 'E' and direction_b == 0:
+            # else:
+            #     exit_point = 1
+            #     break
+    if go_left == 'E' and exit_point != 1:
         while True:
             print(steps)
             if point[a][b + 1] == '.':
                 go_left = 'N'
-                direction_b = 1
                 b += 1
+                if b == -1 and point[a][b] != start:
+                    exit_point = 1
+                    break
                 steps.append([a, b])
-                tupik = 0
                 break
             elif point[a][b + 1] == '#':
-                tupik += 1
+                go_left = 'S'
                 break
-            else:
-                exit_point = 1
-                break
-    if go_left == 'point' or 'W' and direction_b == 0:
+            # else:
+            #     exit_point = 1
+            #     break
+    if go_left == 'W' and exit_point != 1:
         while True:
             print(steps)
             if point[a][b - 1] == '.':
                 go_left = 'S'
-                direction_b = 1
                 b -= 1
+                if b == 0 and point[a][b] != start:
+                    exit_point = 1
+                    break
                 steps.append([a, b])
-                tupik = 0
                 break
             elif point[a][b - 1] == '#':
-                tupik += 1
+                go_left = 'N'
                 break
-            else:
-                exit_point = 1
-                break
+            # else:
+            #     exit_point = 1
+            #     break
     if tupik >= 3:
         point[a][b] = '#'
     if exit_point == 1:
